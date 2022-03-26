6fa102039c7ee44254af1e7b7f2b709c1efb91a8
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-26 10:44:43 -0500

    delete notes function

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
+// Create New Note
 // body = req.body from notesRoutes post route
 // notesArray = notes from notesRoutes which is the data from db.json
 function createNewNote(body, notesArray) {
@@ -14,12 +15,13 @@ function createNewNote(body, notesArray) {
     // save notes array to json file
     fs.writeFileSync(
         path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
+        JSON.stringify(notesArray, null, 2)
     );
   
     return note;
 };
 
+// Validate Note
 // note = req.body from notesRoutes post route
 function validateNote(note) {
     if (!note.title || typeof note.title !== 'string') {
@@ -31,25 +33,18 @@ function validateNote(note) {
     return true;
 };
 
+// Delete Note 
 function deleteById(id, notesArray) {
-
-    // check if a note with that id exists in the notesArray
-    const found = notesArray.find(note => note.id === id);
-
-    // if that note does not exist, return out of function
-    if (!found) {
-        return false;
-    }
-
+    // filter notes array to remove note with id = id
     const deleteArray = notesArray.filter(note => note.id !== id);
 
     // save notes array to json file
     fs.writeFileSync(
         path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: deleteArray }, null, 2)
+        JSON.stringify(deleteArray, null, 2)
     );
 
-    return true;
+    return deleteArray;
 };
 
 module.exports = { createNewNote, validateNote, deleteById };
--- Develop/routes/apiRoutes/notesRoutes.js
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,3 @@
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
 const fs = require('fs');
 const path = require('path');
 const notesRouter = require('express').Router();
@@ -21,7 +7,7 @@ const uniqid = require('uniqid');
 const { createNewNote, validateNote, deleteById } = require('../../lib/notes');
 
 // import notes data
-const { notes } = require('../../db/db');
+const notes = require('../../db/db');
 
 // Get notes
 notesRouter.get('/notes', (req, res) => {
@@ -53,15 +39,10 @@ notesRouter.post('/notes', (req, res) => {
 // Delete notes
 notesRouter.delete('/notes/:id', (req, res) => {
 
-    const noteDeleted = deleteById(req.params.id, notes);
+    deleteById(req.params.id, notes);
 
-    //if the function deleteById returns false
-    if (!noteDeleted) {
-        res.status(400).send('This note does not exist.');
-    }
-    else {
-        res.json(notes);
-    }
+    res.status(200).send('Note deleted.');
+
 });
 // req = will contain information about the call to the server, the path, the query parameters, and maybe path parameters
 // res = will contain information that gets sent back to the client
